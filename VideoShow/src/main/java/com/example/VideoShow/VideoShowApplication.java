b87584ff5061f010f011f24598b6fdf6cd725faf
ganesh2-dev <ganeshkumar22j@gmail.com>
2025-07-21 14:12:21 +0530

    Initial spring boot project videoshow

---
+++ VideoShow/src/main/java/com/example/VideoShow/VideoShowApplication.java
@@ -0,0 +1,13 @@
+package com.example.VideoShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoShowApplication.class, args);
+	}
+
+}
---
+++ VideoShow/src/main/java/com/example/VideoShow/controller/VideoController.java
@@ -0,0 +1,35 @@
+package com.example.VideoShow.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+
+public class VideoController {
+
+    @GetMapping("/video/{filename}")
+    public ResponseEntity<Resource> streamVideo(@PathVariable String filename) throws IOException {
+
+        filename = filename.replaceAll("[<>:\"/\\\\|?*]", "").trim();
+        System.out.println(filename);
+
+        Path videoPath = Paths.get("C:\\Users\\ganes\\Downloads\\VideoShow\\VideoShow\\src\\main\\resources\\videos").resolve(filename).normalize();
+
+        Resource videoResource = new UrlResource(videoPath.toUri());
+
+        if (!videoResource.exists() || !videoResource.isReadable()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("video/mp4"))
+                .body(videoResource);
+    }
+}
